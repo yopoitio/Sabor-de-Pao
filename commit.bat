84668c136c99a0e021db48d5c48f1d40290eb46c
yopoitio <rafa.ba@sapo.pt>
2025-03-02 17:01:51 +0000

    2025-03-02_17-01-51

---
+++ commit.bat
@@ -0,0 +1,11 @@
+@echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+echo fullstamp: "%fullstamp%"
+
+git add --all
+git commit -m "%fullstamp%"
+git push
